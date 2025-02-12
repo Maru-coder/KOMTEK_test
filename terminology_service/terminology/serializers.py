d4bd979fcc801219083e6769ba7bdf44da067cf9
Марина Ежова <marickyarick@gmail.com>
2025-02-12 09:31:52 +0500

    added serializers

--- terminology_service/terminology/serializers.py
+++ terminology_service/terminology/serializers.py
@@ -1,14 +1,15 @@
 from rest_framework import serializers
+
 from .models import Refbook, RefbookElement
 
 
 class RefbookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Refbook
-        fields = ['id', 'code', 'name']
+        fields = ["id", "code", "name"]
 
 
 class RefbookElementSerializer(serializers.ModelSerializer):
     class Meta:
         model = RefbookElement
-        fields = ['code', 'value']
+        fields = ["code", "value"]
