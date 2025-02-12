ded64064d53152e11dae55cc8c44ba41fb0b544d
Марина Ежова <marickyarick@gmail.com>
2025-02-12 09:27:27 +0500

    added common urls and url for swagger

--- terminology_service/terminology_service/urls.py
+++ terminology_service/terminology_service/urls.py
@@ -1,16 +1,15 @@
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib import admin
+from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
         title="Terminology API",
-        default_version='v1',
+        default_version="v1",
         description="API documentation for the Terminology service",
     ),
     public=True,
@@ -18,7 +17,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('terminology.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path("admin/", admin.site.urls),
+    path("api/", include("terminology.urls")),
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
