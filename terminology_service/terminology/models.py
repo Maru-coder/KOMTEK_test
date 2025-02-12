f811e30d26b246cd221fa09ca43dcae3c28f87cc
Марина Ежова <marickyarick@gmail.com>
2025-02-12 09:31:24 +0500

    added models

--- terminology_service/terminology/models.py
+++ terminology_service/terminology/models.py
@@ -15,14 +15,14 @@ class Refbook(models.Model):
 
 
 class RefbookVersion(models.Model):
-    refbook = models.ForeignKey(Refbook, on_delete=models.CASCADE, related_name='versions', verbose_name="Справочник")
+    refbook = models.ForeignKey(Refbook, on_delete=models.CASCADE, related_name="versions", verbose_name="Справочник")
     version = models.CharField(max_length=50, verbose_name="Версия")
     start_date = models.DateField(verbose_name="Дата начала версии")
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['refbook', 'version'], name='unique_refbook_version'),
-            models.UniqueConstraint(fields=['refbook', 'start_date'], name='unique_refbook_start_date'),
+            models.UniqueConstraint(fields=["refbook", "version"], name="unique_refbook_version"),
+            models.UniqueConstraint(fields=["refbook", "start_date"], name="unique_refbook_start_date"),
         ]
         verbose_name = "Версия справочника"
         verbose_name_plural = "Версии справочника"
@@ -32,13 +32,15 @@ class RefbookVersion(models.Model):
 
 
 class RefbookElement(models.Model):
-    version = models.ForeignKey(RefbookVersion, on_delete=models.CASCADE, related_name='elements', verbose_name="Версия справочника")
+    version = models.ForeignKey(
+        RefbookVersion, on_delete=models.CASCADE, related_name="elements", verbose_name="Версия справочника"
+    )
     code = models.CharField(max_length=100, verbose_name="Код элемента")
     value = models.CharField(max_length=300, verbose_name="Значение элемента")
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['version', 'code'], name='unique_version_code'),
+            models.UniqueConstraint(fields=["version", "code"], name="unique_version_code"),
         ]
         verbose_name = "Элемент справочника"
         verbose_name_plural = "Элементы справочника"
