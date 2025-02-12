bd9813add713ef9c7dd399876579041dab3e27b8
Марина Ежова <marickyarick@gmail.com>
2025-02-12 09:26:57 +0500

    added urls for api

--- terminology_service/terminology/urls.py
+++ terminology_service/terminology/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RefbookListView, RefbookElementView, CheckElementView
+
+from .views import CheckElementView, RefbookElementView, RefbookListView
 
 urlpatterns = [
-    path('refbooks/', RefbookListView.as_view(), name='refbooks-list'),
-    path('refbooks/<int:id>/elements/', RefbookElementView.as_view(), name='refbooks-elements'),
-    path('refbooks/<int:id>/check_element/', CheckElementView.as_view(), name='check-element'),
+    path("refbooks/", RefbookListView.as_view(), name="refbooks-list"),
+    path("refbooks/<int:id>/elements/", RefbookElementView.as_view(), name="refbooks-elements"),
+    path("refbooks/<int:id>/check_element/", CheckElementView.as_view(), name="check-element"),
 ]
